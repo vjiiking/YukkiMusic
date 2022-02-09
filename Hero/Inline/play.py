22fd26c78db00b988a5dc297a16568189d7e381e
Lucky boy ❤️ <94621036+Mrvk1703@users.noreply.github.com>
2022-02-09 09:56:20 +0530

    Update play.py

--- Hero/Inline/play.py
+++ Hero/Inline/play.py
@@ -2,7 +2,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 
                             InlineKeyboardMarkup, InputMediaPhoto, Message)
 
-from Yukki import db_mem
+from Hero import db_mem
 
 def url_markup(videoid, duration, user_id, query, query_type):
 
